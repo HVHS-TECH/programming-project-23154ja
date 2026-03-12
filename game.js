cb6fdbf42ed681f6094b25b16b3a54fa78adf014
23154ja <23154ja@hvhs.school.nz>
2026-03-11 21:28:20 -1100

    changed background and started foodfunc

--- game.js
+++ game.js
@@ -1,12 +1,16 @@
-const WORLDX = 6000;
+const WORLDX = 10000;
 
-const WORLDY = 6000;
+const WORLDY = 10000;
 
 const WORMLENGTH = 67;
 
 const WORMWIDTH = 52;
 
 const WORMSPEED = 5;
+
+const FOODSPACING = 1000**2;
+
+const WORLDSEED = 112358;
 
 //p5 play vars
 
@@ -22,21 +26,31 @@ let player;
 
 let playerBorder;
 
+let faceSprite;
+
+let foodGroup;
+
+
 let lastSegment = 0;
 
 let tailSegments = [];
 let tailBorderSegments = [];
 
 
+
+//fix tail not teleporting when hitting wall
+
+// make background crisp
+
 /******************
 preload
 *****************/
-	
+
 function preload() {
 
-  imgBG = loadImage('assets/images/supersushi4.png');
+	imgBG = loadImage('assets/images/background.png');
 
-  imgFace = loadImage('assets/images/face.png');
+	imgFace = loadImage('assets/images/face.png');
 
 }
 
@@ -46,45 +60,57 @@ setup
 *****************/
 
 function setup() {
-	imgBG.resize(6000,6000);
+	frameRate(60);
 
+	randomSeed(WORLDSEED);
 
-	bgSprite = new Sprite(WORLDX/2, WORLDY/2, WORLDX, WORLDY, "n");
+	noSmooth();
+
+	imgBG.resize(WORLDX, WORLDY);
+
+	cnv = new Canvas(windowWidth, windowHeight);
+
+	bgSprite = new Sprite(WORLDX / 2, WORLDY / 2, WORLDX, WORLDY, "n");
 	bgSprite.image = imgBG;
 	bgSprite.depth = 100;
 
-    
-    console.log("setup");
-    frameRate(60);
-
-    cnv = new Canvas(windowWidth, windowHeight);
-
 	wormSetup();
-	
-	camera.x=player.x;
-	camera.y=player.y;
+
+	initalFoodSetup();
+
+	camera.x = player.x;
+	camera.y = player.y;
 
 }
 
 
 function wormSetup() {
-		
-	playerBorder = new Sprite(500, 50, WORMWIDTH);
 
-	playerBorder.strokeWeight=0;
+	playerBorder = new Sprite(500, 1500, WORMWIDTH);
 
-	playerBorder.color="black";
+	playerBorder.strokeWeight = 0;
+
+	playerBorder.color = "black";
 
 	playerBorder.layer = 1;
 
-	
-    player = new Sprite(500, 50, WORMWIDTH-2, "n");
 
-	player.img = imgFace;
+	player = new Sprite(playerBorder.x, playerBorder.y, WORMWIDTH - 2, "n");
 
-	player.strokeWeight=0;
+	player.color = "salmon";
 
-	player.layer = 10;
+	player.strokeWeight = 0;
+
+	player.layer = 9;
+
+
+	faceSprite = new Sprite(playerBorder.x, playerBorder.y, WORMWIDTH - 2, "n");
+
+	faceSprite.img = imgFace;
+
+	faceSprite.strokeWeight = 0;
+
+	faceSprite.layer = 10;
 
 
 
@@ -92,39 +118,48 @@ function wormSetup() {
 
 	for (let i = 1; i <= WORMLENGTH; i++) {
 
-	let tailBorder = new Sprite(player.x+(i-WORMLENGTH)*WORMSPEED, player.y, WORMWIDTH, "n");
+		let tailBorder = new Sprite(player.x + (i - WORMLENGTH) * WORMSPEED, player.y, WORMWIDTH, "n");
 
-	tailBorder.layer = 1;
+		tailBorder.layer = 1;
 
-	tailBorder.color = "black";
+		tailBorder.color = "black";
 
-	tailBorder.strokeWeight=0;
+		tailBorder.strokeWeight = 0;
 
-	tailBorderSegments.push(tailBorder);
+		tailBorderSegments.push(tailBorder);
 
 
 
-	let tail = new Sprite(player.x+(i-WORMLENGTH)*WORMSPEED, player.y, WORMWIDTH-2, "n");
+		let tail = new Sprite(player.x + (i - WORMLENGTH) * WORMSPEED, player.y, WORMWIDTH - 2, "n");
 
-	tail.layer = 2;
+		tail.layer = 2;
 
-	tail.color = "salmon";
+		tail.color = "salmon";
 
-	tail.strokeWeight = 0;
+		tail.strokeWeight = 0;
 
-	tailSegments.push(tail);
+		tailSegments.push(tail);
 
 	}
 }
 
+function initalFoodSetup() {
 
+	foodGroup = new Group();
+
+	for (let i = 0; i < WORLDX*WORLDY/FOODSPACING; i++) {
+
+		let foodItem = new Sprite(random(WORLDX),random(WORLDY), "n");
+		foodGroup.add(foodItem);
+	}
+}
 
 /******************
 drawFunc
 *****************/
 
 function draw() {
-    
+
 	background('green');
 
 	playerMove(WORMSPEED);
@@ -134,20 +169,20 @@ function draw() {
 }
 
 
-function playerMove (speed) {
+function playerMove(speed) {
 
 	let movingX = true;
 	let movingY = true;
 	let xDirection = 0;
 	let yDirection = 0;
-    
+
 	if (kb.pressing('left') && !kb.pressing('right')) {
 		xDirection = -1;
-	} 
+	}
 
 	else if (kb.pressing('right') && !kb.pressing('left')) {
 		xDirection = 1;
-	} 
+	}
 
 	else {
 		playerBorder.vel.x = 0;
@@ -156,14 +191,14 @@ function playerMove (speed) {
 	}
 
 
-	
+
 	if (kb.pressing('up') && !kb.pressing('down')) {
 		yDirection = -1;
-	} 
+	}
 
 	else if (kb.pressing('down') && !kb.pressing('up')) {
 		yDirection = 1;
-	} 
+	}
 
 	else {
 		playerBorder.vel.y = 0;
@@ -174,45 +209,48 @@ function playerMove (speed) {
 
 	if (!movingY) {
 
-		playerBorder.x += xDirection*speed;
+		playerBorder.x += xDirection * speed;
 
 	} else {
 
-		playerBorder.x += xDirection*Math.sqrt(speed**2/2);
+		playerBorder.x += xDirection * Math.sqrt(speed ** 2 / 2);
 
 	}
 
 	if (!movingX) {
 
-		playerBorder.y += yDirection*speed;
+		playerBorder.y += yDirection * speed;
 
 	} else {
 
-		playerBorder.y += yDirection*Math.sqrt(speed**2/2)
+		playerBorder.y += yDirection * Math.sqrt(speed ** 2 / 2)
 
 	}
 
 
 
-	if (Math.abs(playerBorder.x-WORLDX/2)+WORMWIDTH/2>WORLDX/2) {
+	if (Math.abs(playerBorder.x - WORLDX / 2) + WORMWIDTH / 2 > WORLDX / 2) {
 
-		playerBorder.x = WORLDX/2+(playerBorder.x-WORLDX/2)/Math.abs(playerBorder.x-WORLDX/2)*(WORLDX/2-WORMWIDTH/2)
+		playerBorder.x = WORLDX / 2 + (playerBorder.x - WORLDX / 2) / Math.abs(playerBorder.x - WORLDX / 2) * (WORLDX / 2 - WORMWIDTH / 2)
 
 		movingX = false;
 
 	}
 
-		if (Math.abs(playerBorder.y-WORLDY/2)+WORMWIDTH/2>WORLDY/2) {
+	if (Math.abs(playerBorder.y - WORLDY / 2) + WORMWIDTH / 2 > WORLDY / 2) {
 
-		playerBorder.y = WORLDY/2+(playerBorder.y-WORLDY/2)/Math.abs(playerBorder.y-WORLDY/2)*(WORLDY/2-WORMWIDTH/2)
+		playerBorder.y = WORLDY / 2 + (playerBorder.y - WORLDY / 2) / Math.abs(playerBorder.y - WORLDY / 2) * (WORLDY / 2 - WORMWIDTH / 2)
 
 		movingY = false;
 
 	}
 
 
-	player.x=playerBorder.x;
-	player.y=playerBorder.y;
+	player.x = playerBorder.x;
+	player.y = playerBorder.y;
+
+	faceSprite.x = playerBorder.x;
+	faceSprite.y = playerBorder.y;
 
 	if (movingX || movingY) {
 		moveTail();
@@ -227,33 +265,38 @@ function windowResized() {
 
 
 function moveCamera(percentPerFrame) {
-	
-	camera.x += (player.x - camera.x) * (percentPerFrame/100)
-	camera.y += (player.y - camera.y) * (percentPerFrame/100)
 
-	if (Math.abs(camera.x-WORLDX/2)+windowWidth/2>WORLDX/2) {
+	camera.x += (player.x - camera.x) * (percentPerFrame / 100)
+	camera.y += (player.y - camera.y) * (percentPerFrame / 100)
 
-		camera.x = WORLDX/2+(camera.x-WORLDX/2)/Math.abs(camera.x-WORLDX/2)*(WORLDX/2-windowWidth/2)
+	if (Math.abs(camera.x - WORLDX / 2) + windowWidth / 2 > WORLDX / 2) {
+
+		camera.x = WORLDX / 2 + (camera.x - WORLDX / 2) / Math.abs(camera.x - WORLDX / 2) * (WORLDX / 2 - windowWidth / 2)
 
 	}
 
-		if (Math.abs(camera.y-WORLDY/2)+windowHeight/2>WORLDY/2) {
+	if (Math.abs(camera.y - WORLDY / 2) + windowHeight / 2 > WORLDY / 2) {
 
-		camera.y = WORLDY/2+(camera.y-WORLDY/2)/Math.abs(camera.y-WORLDY/2)*(WORLDY/2-windowHeight/2)
+		camera.y = WORLDY / 2 + (camera.y - WORLDY / 2) / Math.abs(camera.y - WORLDY / 2) * (WORLDY / 2 - windowHeight / 2)
 
 	}
 }
 
 
 function moveTail() {
-	tailSegments[lastSegment].x=player.x;
-	tailSegments[lastSegment].y=player.y;
+	tailSegments[lastSegment].x = player.x;
+	tailSegments[lastSegment].y = player.y;
 
-	tailBorderSegments[lastSegment].x=player.x;
-	tailBorderSegments[lastSegment].y=player.y;
+	tailBorderSegments[lastSegment].x = player.x;
+	tailBorderSegments[lastSegment].y = player.y;
 
 	lastSegment++;
-	if (lastSegment==WORMLENGTH) {
-		lastSegment=0;
+	if (lastSegment == WORMLENGTH) {
+		lastSegment = 0;
 	}
+}
+
+
+function spawnFood() {
+
 }
