450c7179aaea38c9dd66bfa676729f3630084be9
23154ja <23154ja@hvhs.school.nz>
2026-03-31 17:08:56 -1100

    70% commented

--- game.js
+++ game.js
@@ -541,35 +541,49 @@ function playerMove(speed) {
 triggers when the window size is altered, allowing for the program to re-adjust and re-center
 *****************/
 function windowResized() {
+	//resize the canvas (basically the window into the world) to the screen size
 	resizeCanvas(windowWidth, windowHeight);
+	//code to run in start screen
 	if (gameState == 'start') {
+		//background image is 1440 by 1080, this checks to see if it is limited horizontally
 		if (windowWidth / 1440 < windowHeight / 1080) {
+			//ajust scale so width = windowwidth
 			overlay.scale = windowWidth / 1440;
 			overlay.x = windowWidth / 2;
+			//keep positioned at the top of the screen
 			overlay.y = overlay.scale * 540;
 		} else {
+			//if background is limited vertically, set height to window height
 			overlay.scale = windowHeight / 1080;
+			//keep positioned in the middle of the screen
 			overlay.x = windowWidth / 2;
 			overlay.y = windowHeight / 2;
 		}
+		//start and help button pos and scale are based on overlay
 		startButton.x = overlay.x - 400 * overlay.scale;
 		startButton.y = overlay.y + 320 * overlay.scale;
 		startButton.scale = startButton.scale = overlay.scale * 1.2;
-
 		helpButton.x = overlay.x + 250 * overlay.scale;
 		helpButton.y = overlay.y + 320 * overlay.scale;
 		helpButton.scale = startButton.scale = overlay.scale * 1.2;
 
+		//code to run in end screen
 	} else if (gameState == 'end') {
+		//background image is 1620 by 1440, this checks to see if it is limited horizontally
 		if (windowWidth / 1620 < windowHeight / 1440) {
+			//ajust scale so width = windowwidth
 			overlay.scale = windowWidth / 1620;
 			overlay.x = windowWidth / 2;
+			//keep positioned at the top of the screen
 			overlay.y = overlay.scale * 720;
 		} else {
+			//if background is limited vertically, set height to window height
 			overlay.scale = windowHeight / 1440;
+			//keep positioned in the middle of the screen
 			overlay.x = windowWidth / 2;
 			overlay.y = windowHeight / 2;
 		}
+		//digit pos and scale are based on overlay
 		digit1.x = overlay.x - 60 * overlay.scale;
 		digit1.y = overlay.y + 215 * overlay.scale;
 		digit1.scale = overlay.scale / 0.56;
@@ -582,7 +596,7 @@ function windowResized() {
 		digit4.x = digit3.x + 90 * overlay.scale;
 		digit4.y = digit1.y;
 		digit4.scale = digit1.scale;
-
+		//reset and home button pos and scale are based on overlay
 		resetButton.x = overlay.x + 100 * overlay.scale;
 		resetButton.y = overlay.y + 500 * overlay.scale;
 		homeButton.x = overlay.x - 400 * overlay.scale;
